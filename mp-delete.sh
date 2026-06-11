0ba15918433501dda26c92cf822b4372c861ec13
Grigory Provodin <93893085+leambeam@users.noreply.github.com>
2026-06-11 19:27:16 +0300

    feat: Add logic to the delete script

--- mp-delete.sh
+++ mp-delete.sh
@@ -1,10 +1,34 @@
-# no args
+#!/usr/bin/env bash
 
-# delete
-# purge
-# remove ip from known_hosts
+set -euo pipefail
 
-# --all 
-# delete the keys dir
-# rm the file
+readonly env_file="/tmp/shared_vars.env"
+
+if [[ ! -f $env_file ]]; then
+	echo "Failed to locate the env file \"$env_file\"."
+	exit 1
+fi
+
+# Ignore that the file doesn't exist at the moment of the lint
+# shellcheck disable=SC1090
+source "$env_file"
+
+if [[ -n "$full_path" ]]; then
+	echo "Removing $full_path"
+	rm -r "$full_path"
+fi
+
+if [[ -n "$cloud_init_path" ]]; then
+	echo "Removing $cloud_init_path"
+	rm "$cloud_init_path"
+fi
+
+if [[ -n "$name" ]]; then
+	ssh-keygen -R "${name}.local"
+	multipass stop "$name"
+	multipass delete "$name"
+	multipass purge
+fi
+
+rm "$env_file"
 
