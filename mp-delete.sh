15e9a6c19eaf2895d95a1c22a711a0b816d51775
Grigory Provodin <93893085+leambeam@users.noreply.github.com>
2026-06-23 21:11:50 +0300

    feat: Modify delete script to handle multiple VMs at a time

--- mp-delete.sh
+++ mp-delete.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# TODO: Add the functionality to handle multiple VMs at once
-
 set -euo pipefail
 
 # Absolute path of directory containing the executed script
@@ -9,44 +7,54 @@ set -euo pipefail
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly script_dir # Declare and assign separately to avoid masking return values (shellcheck SC2155)
 
+readonly ssh_base="${script_dir}/ssh"
+readonly cloud_init_base="${script_dir}/cloud-init"
+
 vm_not_found() {
+    local vm=$1
+    local answer 
     while true; do
-        read -r -p "VM \"$vm_name\" not found on Multipass. Delete any associated local files? (y/n): " answer
+        read -r -p "VM \"$vm\" not found on Multipass. Delete any associated local files? (y/n): " answer
         case "$answer" in
-        y) break ;;
-        n) exit 1 ;;
+        y) return 0 ;;
+        n) return 1 ;;
         *) echo "Invalid choice. Use either y or n." >&2 ;;
         esac
     done
 }
 
-vm_name=${1:-}
+delete() {
+    # Cleanup steps in this function use `|| echo "..."` guardrail to report failures without
+    # interrupting the script (set -e) or skipping remaining VMs/steps. Exit code stays 0.
+    for vm in "$@"; do
+        local vm_key_dir="${ssh_base}/${vm}"
+        local generated_cloud_init_path="${cloud_init_base}/cloud-init-$vm.yaml"
 
-if [[ -z "$vm_name" ]]; then
-    echo "Usage: $0 <vm-name>" >&2
+        if multipass info "$vm" &>/dev/null; then
+            echo "Deleting \"$vm\"..."
+            multipass delete "$vm" --purge || echo "Failed to delete \"$vm\" from Multipass." >&2
+        else
+            vm_not_found "$vm" || continue # skip the rest of the loop if 'vm_not_found' returns 1
+        fi
+
+        ssh-keygen -R "${vm}.local" || echo "Failed to remove \"$vm\" from known hosts." >&2
+
+        if [[ -d "$vm_key_dir" ]]; then
+            echo "Removing $vm_key_dir"
+            rm -r "$vm_key_dir" || echo "Failed to remove \"$vm_key_dir\"." >&2
+        fi
+
+        if [[ -f "$generated_cloud_init_path" ]]; then
+            echo "Removing $generated_cloud_init_path"
+            rm "$generated_cloud_init_path" || echo "Failed to remove  \"$generated_cloud_init_path\"." >&2
+        fi
+    done
+}
+
+if (($# < 1)); then
+    echo "Usage with a single VM: $0 <vm-name>" >&2
+    echo "Usage with multiple VMs: $0 <vm-name-1> <vm-name-2> <vm-name-3>" >&2
     exit 1
 fi
 
-readonly ssh_base="${script_dir}/ssh"
-readonly cloud_init_base="${script_dir}/cloud-init" 
-readonly vm_key_dir="${ssh_base}/${vm_name}"
-readonly generated_cloud_init_path="${cloud_init_base}/cloud-init-$vm_name.yaml"
-
-if multipass info "$vm_name" &> /dev/null; then
-    echo "Deleting \"$vm_name\"..."
-    multipass delete "$vm_name" --purge
-else
-    vm_not_found
-fi
-
-ssh-keygen -R "${vm_name}.local"
-
-if [[ -d "$vm_key_dir" ]]; then
-    echo "Removing $vm_key_dir"
-    rm -r "$vm_key_dir"
-fi
-
-if [[ -f "$generated_cloud_init_path" ]]; then
-    echo "Removing $generated_cloud_init_path"
-    rm "$generated_cloud_init_path"
-fi
+delete "$@"
